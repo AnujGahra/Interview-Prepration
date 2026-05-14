f66c7457313a0d4752e30fceff89f38aeba5f100
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-14 16:31:54 +0530

    How to use Lamda Expression

---
+++ Java 8 Features/SoftwareEngineer.java
@@ -0,0 +1,19 @@
+
+
+
+public class SoftwareEngineer {
+    
+
+    public static void main(String[] args) {
+        // SoftwareEngineer engineer = new SoftwareEngineer();
+        // System.out.println(engineer.getName());
+
+        Employee employee = () -> "Software Engineer";
+        System.out.println(employee.getName());
+    }
+
+    // @Override
+    // public String getName() {
+    //     return "Anuj";
+    // }
+}

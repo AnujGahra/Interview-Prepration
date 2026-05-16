03d6f383e1070569416ee9d90432c3dccecf6e2a
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-16 17:10:23 +0530

    LinkedList

---
+++ DSA/LinkedList/Basicsll.java
@@ -0,0 +1,28 @@
+public class Basicsll {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(6);
+        Node c = new Node(8);
+        Node d = new Node(10);
+        Node e = new Node(15);
+
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        
+        
+    }
+}

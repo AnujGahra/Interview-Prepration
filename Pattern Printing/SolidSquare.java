08ee65e4af2eab932c245b7c41ca0489b51cd43c
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-16 09:57:38 +0530

    Solid Square

---
+++ Pattern Printing/SolidSquare.java
@@ -0,0 +1,14 @@
+public class SolidSquare {
+    
+
+    public static void main(String[] args) {
+        int size = 5;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

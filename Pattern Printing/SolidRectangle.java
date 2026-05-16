277089e7173fdcab85254363e0ad47e762f31129
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-16 09:52:18 +0530

    Solid Rectangle

---
+++ Pattern Printing/SolidRectangle.java
@@ -0,0 +1,15 @@
+public class SolidRectangle {
+    
+
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 6;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

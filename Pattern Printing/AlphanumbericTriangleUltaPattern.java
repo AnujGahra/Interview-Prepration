50dc968a4d9d53613039a2e956adf5fb75bac6a6
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-16 10:43:05 +0530

    AlphanumbericTriangleUltaPattern

---
+++ Pattern Printing/AlphanumbericTriangleUltaPattern.java
@@ -0,0 +1,21 @@
+public class AlphanumbericTriangleUltaPattern {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print((char)(70 - i) + " ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}

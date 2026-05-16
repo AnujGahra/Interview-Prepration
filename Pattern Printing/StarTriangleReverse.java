9032d8b84b30a4d7e799c24d8ebc45c7c8957263
Anuj Kumar <anujgahra244304@gmail.com>
2026-05-16 13:00:21 +0530

    StarTriangleReverse

--- Pattern Printing/StarTriangleReverse.java
+++ Pattern Printing/StarTriangleReverse.java
@@ -4,15 +4,26 @@ public class StarTriangleReverse {
     public static void main(String[] args) {
         int size = 5;
 
+        // for (int i = 1; i <= size; i++) {
+        //     for (int j = 1; j <= size; j++) {
+        //         if((i+j) <= size) {
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
         for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                if((i+j) <= size) {
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
-                }
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
             }
-            System.out.println();
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }  
+            System.out.println(); 
         }
     }
 }

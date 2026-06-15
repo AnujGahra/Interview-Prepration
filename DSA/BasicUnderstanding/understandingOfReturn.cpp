14459c18e7a230c49f322889160e36396ea93e5e
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-16 00:05:48 +0530

    understanding of return statement

--- DSA/BasicUnderstanding/understandingOfReturn.cpp
+++ DSA/BasicUnderstanding/understandingOfReturn.cpp
@@ -6,5 +6,6 @@ int main()
     cout<<"The value of a is "<<a<<endl;
     // return 0; // tells the OS that the program executed successfully.
     // return; // this will cause a compilation error because the main function is expected to return an int value.
-    return 1; // Usually indicates an error or abnormal termination.
+    // return 1; // Usually indicates an error or abnormal termination.
+    return 3; // This will return the value 3 to the operating system, which can be used to indicate a specific status or error code.
 }

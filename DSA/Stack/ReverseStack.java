672a00890666f4f7247fc1a050327829f9c3b86c
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-18 19:50:23 +0530

    concept of Under Flow and Over Flow

--- DSA/Stack/ReverseStack.java
+++ DSA/Stack/ReverseStack.java
@@ -3,8 +3,27 @@ import java.util.Stack;
 public class ReverseStack {
 
 
-    
-    
+    public static void pushAtBottom(Stack<Integer> st, int x) {
+        if(st.size() == 0) {
+            st.push(x);
+            return;
+        }
+
+        int top = st.pop();
+        pushAtBottom(st, x);
+        st.push(top);
+    }
+
+     
+    public static void reverseStack(Stack<Integer> st) {
+        if(st.size() == 1) return;
+        int top = st.pop();
+        reverseStack(st);
+        pushAtBottom(st, top);
+    }
+
+
+
 
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
@@ -14,21 +33,24 @@ public class ReverseStack {
         st.push(40);
         System.out.println("Original Stack: " + st);
 
-        Stack<Integer> rt = new Stack<>();
-        while(st.size() > 0) {
-            rt.push(st.pop());
-        }
-        System.out.println("Reversed Stack: " + rt);
+        // Stack<Integer> rt = new Stack<>();
+        // while(st.size() > 0) {
+        //     rt.push(st.pop());
+        // }
+        // System.out.println("Reversed Stack: " + rt);
 
-        Stack<Integer> qt = new Stack<>();
-        while(rt.size() > 0) {
-            qt.push(rt.pop());
-        }
-        System.out.println("New Stack: " + qt);
+        // Stack<Integer> qt = new Stack<>();
+        // while(rt.size() > 0) {
+        //     qt.push(rt.pop());
+        // }
+        // System.out.println("New Stack: " + qt);
 
-        while(qt.size() > 0) {
-            st.push(qt.pop());
-        }
-        System.out.println("Stack after moving elements back: " + st);
+        // while(qt.size() > 0) {
+        //     st.push(qt.pop());
+        // }
+        // System.out.println("Stack after moving elements back: " + st);
+
+        reverseStack(st);
+        System.out.println("Reversed Stack: " + st);
     }
 }

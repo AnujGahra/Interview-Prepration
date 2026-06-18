88c277378f1e7f79223a434d9634834b02ddcb2f
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-18 13:07:31 +0530

    Basic of stack operations

--- DSA/Stack/BasicsofStacks.java
+++ DSA/Stack/BasicsofStacks.java
@@ -10,7 +10,16 @@ public class BasicsofStacks {
         stack.push(30); 
         stack.push(40);
         System.out.println("Stack: " + stack);
+
+        // size
+        System.out.println("Stack size: " + stack.size());
         // peek
         System.out.println("Top element: " + stack.peek());
+        // pop
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack after pop: " + stack);
+
+        // size
+        System.out.println("Stack size after pop: " + stack.size());
     }
 }

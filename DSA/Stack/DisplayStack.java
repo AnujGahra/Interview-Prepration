be8fde703db14875fa5e2bc8c06dc20d41eac2f1
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-18 16:15:37 +0530

    Display stack using array and another stack

--- DSA/Stack/DisplayStack.java
+++ DSA/Stack/DisplayStack.java
@@ -1,6 +1,21 @@
 import java.util.Stack;
 
 public class DisplayStack {
+
+    public static void displayRec(Stack<Integer> st) {
+        if(st.size() == 0) {
+            return;
+        }
+        // st.push(50); // stack is Pass by Reference
+        int top = st.pop();
+        
+        System.out.print(top + " ");
+        displayRec(st);
+        st.push(top);
+
+    }
+
+    
     public static void main(String[] args) {
             Stack<Integer> st = new Stack<>();
             st.push(10);
@@ -9,32 +24,40 @@ public class DisplayStack {
             st.push(40);
             System.out.println("Stack: " + st);
 
-            Stack<Integer> rt = new Stack<>();
-            while(st.size() > 0) {
-                rt.push(st.pop());
-            }
+            displayRec(st);
+            // System.out.println("Stack after display: " + st);
 
-            while(rt.size() > 0) {
-                int x = rt.pop();
-                System.out.print(x + " ");
-                st.push(x);
-            }
-            System.out.println();
-            System.out.println(" stack: " + st);
+            // Stack<Integer> rt = new Stack<>();
+            // while(st.size() > 0) {
+            //     rt.push(st.pop());
+            // }
+
+            // while(rt.size() > 0) {
+            //     int x = rt.pop();
+            //     System.out.print(x + " ");
+            //     st.push(x);
+            // }
+            // System.out.println();
+            // System.out.println(" stack: " + st);
 
 
-            // using array
-            int n = st.size();
-            int[] arr = new int[n];
-            for(int i = n-1; i>=0; i--) {
-                arr[i] = st.pop();
-            }
+            // // using array
+            // int n = st.size();
+            // int[] arr = new int[n];
+            // for(int i = n-1; i>=0; i--) {
+            //     arr[i] = st.pop();
+            // }
 
-            for(int i = 0; i<n; i++) {
-                System.out.print(arr[i] + " ");
-                st.push(arr[i]);
-            }
-            System.out.println();
-            System.out.println(" stack: " + st);
+            // for(int i = 0; i<n; i++) {
+            //     System.out.print(arr[i] + " ");
+            //     st.push(arr[i]);
+            // }
+            // System.out.println();
+            // System.out.println(" stack: " + st);
+
+
+
     }
+
+    
 }

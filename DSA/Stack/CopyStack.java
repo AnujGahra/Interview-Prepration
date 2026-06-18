69a05e43ccef5a7bf8b0d1872df26883759ba080
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-18 15:16:18 +0530

    Copy Stack element in another stack

--- DSA/Stack/CopyStack.java
+++ DSA/Stack/CopyStack.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class CopyStack {
     
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> st = new Stack<>();
+
+        int n;
+        System.out.println("Enter the number elements you want to push into the stack: ");
+        n = sc.nextInt();
+        System.out.println("Enter the elements you want to push into the stack: ");
+        for(int i = 0; i<n; i++) {
+            int x = sc.nextInt();
+            st.push(x);
+        }
+
+        System.out.println("Original stack: " + st);
+    }
 }

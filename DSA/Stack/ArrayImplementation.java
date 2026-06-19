1d526e149435b54e69119bfb75ddec84258cac50
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-19 12:06:15 +0530

    peek method implementation

--- DSA/Stack/ArrayImplementation.java
+++ DSA/Stack/ArrayImplementation.java
@@ -2,9 +2,21 @@ public class ArrayImplementation {
 
     public static class Stack {
         private int[] arr = new int[5];
+
+        private int idx = 0;
+
+
+        void push(int x) {
+            arr[idx] = x;
+            idx++;
+        }
+
+        int peek() {
+            return arr[idx - 1];
+        }
     }
     
-
+ 
     public static void main(String[] args) {
 
     }

3d23e6cb68eb718d128bd7a3555d95837ad626d7
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-19 11:26:19 +0530

    Stack implementation using array

--- DSA/Stack/ArrayImplementation.java
+++ DSA/Stack/ArrayImplementation.java
@@ -1,3 +1,11 @@
 public class ArrayImplementation {
+
+    public static class Stack {
+        private int[] arr = new int[5];
+    }
     
+
+    public static void main(String[] args) {
+
+    }
 }

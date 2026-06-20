f86c2bbe70adb52228651f6f16a089ac1813ba29
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-20 16:56:52 +0530

    custom method for find string length using for loop

--- C++/string.cpp
+++ C++/string.cpp
@@ -15,9 +15,17 @@ bool compare(string str1, string str2) {
 
 
 // custom logic for find string length
+// int stringLength(string str) {
+//     int length = 0;
+//     for (char c : str) {
+//         length++;
+//     }
+//     return length;
+// }
+
 int stringLength(string str) {
     int length = 0;
-    for (char c : str) {
+    for(int i = 0; i < str.size(); i++) {
         length++;
     }
     return length;

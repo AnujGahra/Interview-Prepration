9d3ee458431de6ccf4605e327623114085104132
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-23 22:52:07 +0530

    Arrays + Linear Search

---
+++ C++/CPP_Developer_Interview_Practice/ArraySearch.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i< n; i++) {
+        cin >> arr[i];
+    };
+
+    int x;
+    cout << "Enter the element to search: ";
+    cin >> x;
+    // i need to return the index of the element if found, otherwise return -1
+    int index = -1;
+    for(int i = 0; i<n; i++) {
+        if(arr[i] == x) {
+            index = i;
+            break;
+        }
+    }
+
+    
+    cout << "Index of the element: " << index << endl;
+}

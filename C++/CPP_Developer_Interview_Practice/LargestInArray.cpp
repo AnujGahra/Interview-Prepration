4bbde4e28e5f94ed60cdddb884982cb73cffed81
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-24 16:30:55 +0530

    Largest element in the array

--- C++/CPP_Developer_Interview_Practice/LargestInArray.cpp
+++ C++/CPP_Developer_Interview_Practice/LargestInArray.cpp
@@ -3,20 +3,28 @@ using namespace std;
 
 int main() {
 
-    int arr[] = {20, 30, 40, 50 , 60};
+    // int arr[] = {20, 30, 40, 50 , 60};
 
+    // int n = sizeof(arr)/sizeof(arr[0]);
+
+    // int largestElement = INT_MIN;
+
+    // for(int i = 0; i< n; i++) {
+    //     if(arr[i] > largestElement) {
+    //         // largestElement = i; // for return index
+    //         largestElement = arr[i]; // for return element
+    //     }
+    // }
+
+    // cout << largestElement << endl;
+
+
+    int arr[] = {40, 10, 90, 70, 10};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    int largestElement = INT_MIN;
+    sort(arr, arr+n);
 
-    for(int i = 0; i< n; i++) {
-        if(arr[i] > largestElement) {
-            // largestElement = i; // for return index
-            largestElement = arr[i]; // for return element
-        }
-    }
-
-    cout << largestElement << endl;
+    cout << arr[n-1] << endl;
 
 
     return 0;

3e7a48b6395bbb124d5b3ada6475d447d165b1a1
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-25 11:23:50 +0530

    Practice

---
+++ C++/CPP_Developer_Interview_Practice/Program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int x = 0;
+    if(x = 5) {
+        cout << "case 1" << endl;
+    } else {
+        cout << "case 2" << endl;
+    }
+
+
+    return 0;
+}

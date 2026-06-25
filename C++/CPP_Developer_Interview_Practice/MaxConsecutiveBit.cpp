a2d80a2a40c4a10099db95af414c2359ccc2fdec
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-25 16:23:27 +0530

    Max Consecutive Bit

---
+++ C++/CPP_Developer_Interview_Practice/MaxConsecutiveBit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {0, 1, 0, 1, 1, 1, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int ans = 1;
+    int count = 1;
+
+    for(int i = 0; i<n; i++) {
+        if(arr[i] == arr[i - 1]) count++;
+        else count = 1;
+
+        ans = max(ans, count);
+    }
+
+    cout << ans << endl;
+}

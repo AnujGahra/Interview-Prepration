d5c74a675be2d390efa527e8a5713dc9d0bb1e49
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-25 15:02:30 +0530

    Reverse an array using Swap function

--- C++/CPP_Developer_Interview_Practice/ReverseArray.cpp
+++ C++/CPP_Developer_Interview_Practice/ReverseArray.cpp
@@ -7,7 +7,21 @@ int main() {
 
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(int i = n-1; i>=0; i--) {
+    // for(int i = n-1; i>=0; i--) {
+    //     cout << arr[i] << " ";
+    // }
+
+    // using swap
+    int i = 0;
+
+    int j = n - 1;
+
+    while( i < j) {
+        swap(arr[i++], arr[j--]);
+        
+    }
+
+    for(int i = 0; i<n; i++) {
         cout << arr[i] << " ";
     }
 

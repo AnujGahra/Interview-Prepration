1e386297056e011d94f5ecf5af008a103fd41cfc
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-27 10:55:02 +0530

    Remove Duplicate Element

--- C++/CPP_Developer_Interview_Practice/RemoveDuplicateElement.cpp
+++ C++/CPP_Developer_Interview_Practice/RemoveDuplicateElement.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 
+
+ vector<int> removeDuplicates(vector<int> &arr) {
+        // code here
+        
+        int n = arr.size();
+        
+        if(n == 0) return arr;
+        
+        int j = 0;
+        
+        for(int i = 1; i<n; i++) {
+            if(arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+        
+        arr.resize(j + 1);
+        
+        
+        return arr;
+        
+    }
+
 int main() {
     // int arr[] = {2, 2, 2, 2, 2, 3, 4, 5};
     int arr[] = {1, 2, 4};

2a6a0868567f01c5ee3907ab23a1553e53383d06
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-28 15:55:27 +0530

    Addition of two square matrices

---
+++ C++/CPP_Developer_Interview_Practice/AdditionTwoMetric.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void AddTwoVec(vector<int> &v1, vector<int> &v2) {
+    int n = v1.size();
+    int m = v2.size();
+
+    if (n != m) {
+        cout << "Vectors must be of same size!" << endl;
+        return;
+    }
+
+    vector<int> result;
+
+    for (int i = 0; i < n; i++) {
+        result.push_back(v1[i] + v2[i]);
+    }
+
+    cout << "Sum of vectors: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+}
+
+int main() {
+    vector<int> v1 = {1, 2, 3, 4};
+    vector<int> v2 = {5, 6, 7, 8};
+
+    AddTwoVec(v1, v2);
+
+    return 0;
+}

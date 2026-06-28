4df2fcf1af9e27d44a3c166c3b5da4c51ed717d5
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-28 15:41:24 +0530

    Stock Buy and Sell – Max one Transaction Allowed

--- C++/CPP_Developer_Interview_Practice/StockSellAndBuy.cpp
+++ C++/CPP_Developer_Interview_Practice/StockSellAndBuy.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int maxProfit(int price[], int n) {
+    
+
+    int maxProfit = 0, bestBuy = price[0];
+
+    for(int i = 1; i<n; i++) {
+        if(price[i] > bestBuy) {
+            maxProfit = max(maxProfit, price[i] - bestBuy);
+        }
+        bestBuy = min(bestBuy, price[i]);
+    }
+
+    return maxProfit;
+}
+
 int main() {
     int arr[] = {7, 10, 1, 3, 6, 9, 2};
 
-    
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+
+    int result = maxProfit(arr, n);
+    cout << "MaxProfit: " << result << endl;
+
+    return 0;
 }

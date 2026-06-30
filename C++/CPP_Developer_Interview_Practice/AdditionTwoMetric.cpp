a9608ab864d402a5871e533756300743c992c2f6
Anuj Kumar <anujgahra244304@gmail.com>
2026-06-30 23:52:30 +0530

    Number

--- C++/CPP_Developer_Interview_Practice/AdditionTwoMetric.cpp
+++ C++/CPP_Developer_Interview_Practice/AdditionTwoMetric.cpp
@@ -2,33 +2,39 @@
 #include <vector>
 using namespace std;
 
-void AddTwoVec(vector<int> &v1, vector<int> &v2) {
+void AddTwoVec(vector<int> &v1, vector<int> &v2)
+{
     int n = v1.size();
     int m = v2.size();
 
-    if (n != m) {
+    if (n != m)
+    {
         cout << "Vectors must be of same size!" << endl;
         return;
     }
 
     vector<int> result;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         result.push_back(v1[i] + v2[i]);
     }
 
     cout << "Sum of vectors: ";
-    for (int x : result) {
+    for (int x : result)
+    {
         cout << x << " ";
     }
-
-    
 }
 
-int main() {
+int main()
+{
     vector<int> v1 = {1, 2, 3, 4};
     vector<int> v2 = {5, 6, 7, 8};
 
+    // function calling
+
+    AddTwoVec(v1, v2);
     AddTwoVec(v1, v2);
 
     return 0;
--- C++/CPP_Developer_Interview_Practice/prinAllPrime.cpp
+++ C++/CPP_Developer_Interview_Practice/prinAllPrime.cpp
@@ -1,19 +1,23 @@
-#include<iostream> 
+#include <iostream>
 using namespace std;
 
+int main()
+{
 
-int main() {
-    
-    for(int num = 2; num <=100; num++) {
+    for (int num = 2; num <= 100; num++)
+    {
         bool isPrime = true;
-        for(int i = 2; i*i <= 100; i++) {
-            if(num % i == 0) {
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
                 isPrime = false;
                 break;
             }
         }
 
-        if(isPrime){
+        if (isPrime)
+        {
             cout << num << " ";
         }
     }

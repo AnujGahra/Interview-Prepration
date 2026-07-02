de214e29fb241e54230f8bba3f872a7bb8b01989
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-02 22:06:55 +0530

    Check And Print Neon Number in a Given Range

--- C++/CPP_Developer_Interview_Practice/NeonNumber.cpp
+++ C++/CPP_Developer_Interview_Practice/NeonNumber.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int checkNeon(int num) {
+int checkNeon(int num)
+{
     int sqr = num * num;
 
-
     int sum = 0;
-    while(sqr != 0) {
+    while (sqr != 0)
+    {
         sum += sqr % 10;
         sqr /= 10;
     }
     return (sum == num);
 }
 
-int main() {
+int main()
+{
 
-
-     // Printing Neon Numbers upto 10000
-    for (int i = 1; i <= 10000; i++) 
+    // Printing Neon Numbers upto 10000
+    for (int i = 1; i <= 10000; i++)
         if (checkNeon(i))
-            cout << i << " "; 
+            cout << i << " ";
 
     return 0;
 }

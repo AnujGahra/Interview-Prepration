23d2c22180d8c29a1ffc36255dcf575bf138d23e
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-04 12:22:46 +0530

    Find Largest Among Three Numbers

--- C++/CPP_Developer_Interview_Practice/LargestNumber.cpp
+++ C++/CPP_Developer_Interview_Practice/LargestNumber.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a = 1, b = 2, c = 11;
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 11;
 
     // Finding the largest by comparing using
-  	// relational operators with if-else
-    if (a >= b) {
+    // relational operators with if-else
+    if (a >= b)
+    {
         if (a >= c)
             cout << a;
         else
             cout << c;
     }
-    else {
+    else
+    {
         if (b >= c)
             cout << b;
         else

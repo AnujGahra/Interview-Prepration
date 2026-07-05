089b7a902b19e5db5996b171aa5646f7a3916bc4
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-05 22:29:40 +0530

    Find Largest Number Using Temporary Variable

--- C++/CPP_Developer_Interview_Practice/LargestNumber.cpp
+++ C++/CPP_Developer_Interview_Practice/LargestNumber.cpp
@@ -9,19 +9,29 @@ int main()
 
     // Finding the largest by comparing using
     // relational operators with if-else
-    if (a >= b)
-    {
-        if (a >= c)
-            cout << a;
-        else
-            cout << c;
-    }
-    else
-    {
-        if (b >= c)
-            cout << b;
-        else
-            cout << c;
-    }
+    // if (a >= b)
+    // {
+    //     if (a >= c)
+    //         cout << a;
+    //     else
+    //         cout << c;
+    // }
+    // else
+    // {
+    //     if (b >= c)
+    //         cout << b;
+    //     else
+    //         cout << c;
+    // }
+
+
+    // Using third Variable to store the largest number
+    int temp;
+
+    temp = a;
+    if(b > temp) temp = b;
+    if(c > temp) temp = c;
+
+    cout << "Largest number is: " << temp;
     return 0;
 }

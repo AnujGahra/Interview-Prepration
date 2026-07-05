829197a1059f9225e35f6cc8f8a698755508247d
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-05 23:21:24 +0530

    Factorial using Recursion

--- C++/CPP_Developer_Interview_Practice/Factorial1.cpp
+++ C++/CPP_Developer_Interview_Practice/Factorial1.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 
+int fact(int n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+
 
 int main() {
     int n = 5;
@@ -10,5 +18,8 @@ int main() {
         factorial *= i;
     }
     cout << "Factorial of " << n << " is: " << factorial;
+
+    // using recursion
+    cout << "\nFactorial of " << n << " using recursion is: " << fact(n);
     return 0;
 }

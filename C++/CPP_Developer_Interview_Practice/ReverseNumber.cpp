0c2bfb03550226f518d189d118c979b6d49a2c72
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-05 22:59:49 +0530

    Reverse a Number

---
+++ C++/CPP_Developer_Interview_Practice/ReverseNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int n = 34567;
+    int reversedNumber = 0;
+    while(n != 0) {
+        reversedNumber = reversedNumber * 10 + n % 10;
+        n /= 10;
+    }
+    cout << "Reversed Number is: " << reversedNumber;
+
+    return 0;
+}

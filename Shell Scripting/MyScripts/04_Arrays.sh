64dae38a654348492747b0ea1fdc66a1df2eb366
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-08 15:18:00 +0530

    How to get specific values from Array

--- Shell Scripting/MyScripts/04_Arrays.sh
+++ Shell Scripting/MyScripts/04_Arrays.sh
@@ -4,3 +4,7 @@ myArray=(1 20 30.5 Hello "Hey Buddy!!" )
 echo "Value of first Index: ${myArray[0]}"
 #Print all Array Values
 echo "All the values in array are ${myArray[*]}"
+
+#How to get Specific values?
+echo "${myArray[*]:1}"
+echo "${myArray[*]:1:2}"

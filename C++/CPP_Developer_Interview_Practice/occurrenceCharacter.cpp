31e59dd057a91ebee5ec0aa41b52cd33e246e8ee
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-09 23:41:41 +0530

    Occurance Character

--- C++/CPP_Developer_Interview_Practice/occurrenceCharacter.cpp
+++ C++/CPP_Developer_Interview_Practice/occurrenceCharacter.cpp
@@ -5,24 +5,37 @@ using namespace std;
 int main()
 {
     string str;
-    char ch;
-    int count = 0;
+    // char ch;
+    // int count = 0;
 
     cout << "Enter a string: ";
     getline(cin, str);
 
-    cout << "Enter character to find: ";
-    cin >> ch;
+    // cout << "Enter character to find: ";
+    // cin >> ch;
 
-    for (int i = 0; i < str.length(); i++)
-    {
-        if (str[i] == ch)
-        {
-            count++;
-        }
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     if (str[i] == ch)
+    //     {
+    //         count++;
+    //     }
+    // }
+
+    // cout << "Occurrence of '" << ch << "' = " << count << endl;
+
+
+
+    unordered_map<char, int> freq;
+
+    for (char ch : str) {
+        freq[ch]++;
     }
 
-    cout << "Occurrence of '" << ch << "' = " << count << endl;
+    cout << "\nCharacter Occurrences:\n";
+    for (auto it : freq) {
+        cout << it.first << " : " << it.second << endl;
+    }
 
     return 0;
 }

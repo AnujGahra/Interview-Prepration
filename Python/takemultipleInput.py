4304861828f6752a58b0402f1db881c154e39e3f
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-17 00:46:47 +0530

    Take multipe input in one line

---
+++ Python/takemultipleInput.py
@@ -0,0 +1,6 @@
+# x, y = int(input("Enter the number of rows: ")), int(input("Enter the number of columns: "))
+
+x, y = int(input("Enter the number")).split()
+
+print("The number of rows is:", x)
+print("The number of columns is:", y)

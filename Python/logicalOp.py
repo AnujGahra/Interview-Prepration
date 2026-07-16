89cd10adf68fad7adafd424018cac8d904a6f147
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-17 00:48:54 +0530

    Logical Operator

---
+++ Python/logicalOp.py
@@ -0,0 +1,9 @@
+age = 18
+
+if age >= 18 and age <= 65:
+    print("You are eligible to work.")
+elif age < 18:
+    print("You are not eligible to work.")
+else:
+    print("You are retired.")
+    

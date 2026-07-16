ae9a121f5770704283e5ac34eb07380d15b91471
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-16 16:58:55 +0530

    Count method in string

--- Python/printLength.py
+++ Python/printLength.py
@@ -2,4 +2,7 @@ name = input("Enter your name: ")
 print ("Length of your name is:", len(name))
 
 
-# 
+# WAP to find occurrence of $ in a string
+string = input("Enter a string: ")
+count = string.count('$')
+print("The occurrence of '$' in the string is:", count)

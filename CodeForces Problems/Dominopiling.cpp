a7fafe62b27fa6aacba0d0a295c7963cf33e79c5
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-17 15:24:21 +0530

    feat(codeforces): solve 50A - Domino Piling

---
+++ CodeForces Problems/Dominopiling.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    int areaRectangularBoard = m * n; // Calculate the area of the rectangular board
+
+    int areaDomino = 2; // Each domino covers an area of 2
+
+    int numberOfDominos = areaRectangularBoard / areaDomino; // Calculate the number of dominos needed
+
+    cout << numberOfDominos << endl;
+
+    return 0;
+}

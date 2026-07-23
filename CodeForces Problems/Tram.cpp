2f7cf4223ccf97db629b74a61ef1cd9a69205c05
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-23 11:45:46 +0530

    Implement peak capacity tracker for Tram solution

--- CodeForces Problems/Tram.cpp
+++ CodeForces Problems/Tram.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
 
-
 int main() {
-    
+    int n;
+    int a, b;
+    cin >> n;
+
+    int capacityTram = 0;
+    int ans = 0;
+
+    for(int i = 1; i<=n; i++) {
+        cin >> a >> b;
+
+        // Print a single integer denoting the minimum possible capacity of the tram (0 is allowed).
+
+        capacityTram = capacityTram - a + b;
+
+        ans = max(ans, capacityTram);
+    }
+
+    cout << ans << endl;
+
+    return 0;
 }

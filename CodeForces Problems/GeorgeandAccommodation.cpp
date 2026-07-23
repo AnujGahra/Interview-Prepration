b6143cd7c3427c440bc97da01ef1726fe5065b12
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-23 12:41:15 +0530

    Implement room capacity check for Codeforces 467A

--- CodeForces Problems/GeorgeandAccommodation.cpp
+++ CodeForces Problems/GeorgeandAccommodation.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-    
+
+    int n;
+    cin >> n;
+
+    int p, c;
+
+    int count = 0;
+    for(int i = 0; i<n; i++) {
+        cin >> p >> c;
+        if(c - p >= 2) count++;
+    }
+    cout << count << endl;
+
+    return 0;
 }

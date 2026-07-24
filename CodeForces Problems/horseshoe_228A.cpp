a4381e4b89318197d138348b5035e8576f6f1219
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-24 17:06:38 +0530

    Solve Codeforces 228A (Is your horseshoe on the other hoof?)

--- CodeForces Problems/horseshoe_228A.cpp
+++ CodeForces Problems/horseshoe_228A.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<set>
 using namespace std;
 
 int main() {
+
+    
+
+    set<int> unique_colors;
+    
+    for (int i = 0; i < 4; ++i) {
+        int color;
+        cin >> color;
+        unique_colors.insert(color); // Set will only keep unique values
+    }
+    
+    // Total needed (4) minus unique colors available
+    cout << 4 - unique_colors.size() << endl;
 
 
 

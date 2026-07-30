5fed8c97988e577fbdb74254a2e195a755797d3f
Anuj Kumar <anujgahra244304@gmail.com>
2026-07-30 18:57:56 +0530

    Sum of digits

--- CodeForces Problems/BuyingCPU.cpp
+++ CodeForces Problems/BuyingCPU.cpp
@@ -1,10 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-
 int main() {
+    
 
+    int t;
+    if (!(cin >> t)) return 0;
 
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> ans;
+        int placeValue = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                ans.push_back(digit * placeValue);
+            }
+            n /= 10;
+            placeValue *= 10;
+        }
+
+        cout << ans.size() << "\n";
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << (i == ans.size() - 1 ? "" : " ");
+        }
+        cout << "\n";
+    }
 
     return 0;
 }

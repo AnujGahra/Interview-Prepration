4f93ecd1897fd153a944adc8f74b23cf6421d722
Anuj Kumar <anujgahra244304@gmail.com>
2026-08-14 22:59:15 +0530

    Codeforces: Vasya the Hipster problem solve

---
+++ CodeForces Problems/VasyatheHipster_581A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int r, b;
+    cin >> r >> b;
+
+    int differentSocks = min(r, b);
+    int sameSocks = abs(r - b) / 2;
+
+    cout << differentSocks << " " << sameSocks << endl;
+    return 0;
+}

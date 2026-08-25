3092bc4a2cd9e907a27fe306f16f0291538d7bb1
Anuj Kumar <anujgahra244304@gmail.com>
2026-08-25 15:37:03 +0530

    feat: add C++ solution for finding the minimum moves to reach target

--- CodeForces Problems/YetAnotherTwoIntegersProblem_1409A.cpp
+++ CodeForces Problems/YetAnotherTwoIntegersProblem_1409A.cpp
@@ -3,24 +3,26 @@
 
 using namespace std;
 
-void solve() {
+void solve()
+{
     long long a, b;
     cin >> a >> b;
-    
+
     long long diff = abs(a - b);
     long long moves = (diff + 9) / 10;
-    
+
     cout << moves << "\n";
 }
 
-int main() {
-    
-    
+int main()
+{
+
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
-    
+
     return 0;
 }

b7eef06c21e27c7ef62a0ff13af793df7475acc5
Anuj Kumar <anujgahra244304@gmail.com>
2026-08-30 18:49:59 +0530

    feat: add C++ solution for Dislike of Threes

--- CodeForces Problems/DislikeofThrees_1560A.cpp
+++ CodeForces Problems/DislikeofThrees_1560A.cpp
@@ -2,27 +2,32 @@
 
 using namespace std;
 
-void solve() {
+void solve()
+{
     int k;
     cin >> k;
     int current = 0;
     int count = 0;
-    while (count < k) {
+    while (count < k)
+    {
         current++;
-        if (current % 3 != 0 && current % 10 != 3) {
+        if (current % 3 != 0 && current % 10 != 3)
+        {
             count++;
         }
     }
     cout << current << "\n";
 }
 
-int main() {
-    
+int main()
+{
+
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
-    
+
     return 0;
 }

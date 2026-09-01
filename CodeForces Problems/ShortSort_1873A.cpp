90f6bd30bf1d3af06934d040bccf5772552fb7a4
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-01 23:22:31 +0530

    feat: add C++ solution for Short Sort

---
+++ CodeForces Problems/ShortSort_1873A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s[0] == 'a' || s[1] == 'b' || s[2] == 'c')
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

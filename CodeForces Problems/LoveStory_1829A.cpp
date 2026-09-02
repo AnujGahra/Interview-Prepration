d09e69de0fa01fc051009a099576877f4ffb434a
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-02 20:32:17 +0530

    feat: add C++ solution for Love Story

---
+++ CodeForces Problems/LoveStory_1829A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    string target = "codeforces";
+    int diff = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (s[i] != target[i])
+        {
+            diff++;
+        }
+    }
+    cout << diff << "\n";
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

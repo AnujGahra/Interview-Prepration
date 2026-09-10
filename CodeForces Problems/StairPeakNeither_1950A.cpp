d3c062cbfef821790a0eb92e204e19caec9ff99f
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-10 19:01:01 +0530

    feat: add C++ solution for Stair, Peak, or Neither

---
+++ CodeForces Problems/StairPeakNeither_1950A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < b && b < c)
+    {
+        cout << "STAIR\n";
+    }
+    else if (a < b && b > c)
+    {
+        cout << "PEAK\n";
+    }
+    else
+    {
+        cout << "NONE\n";
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}

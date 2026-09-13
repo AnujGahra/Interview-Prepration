2a2b0eb34b5f7f87981a0f3ec022bf104efd7526
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-13 23:34:33 +0530

    feat: add C++ solution for Mishka and Game

---
+++ CodeForces Problems/MishkaandGame_703A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int mishka_wins = 0;
+    int chris_wins = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int m, c;
+        cin >> m >> c;
+        if (m > c)
+        {
+            mishka_wins++;
+        }
+        else if (c > m)
+        {
+            chris_wins++;
+        }
+    }
+
+    if (mishka_wins > chris_wins)
+    {
+        cout << "Mishka\n";
+    }
+    else if (chris_wins > mishka_wins)
+    {
+        cout << "Chris\n";
+    }
+    else
+    {
+        cout << "Friendship is magic!^^\n";
+    }
+
+    return 0;
+}
